beb4dec52b19c88987f1a003346474980d5d1ec8
OnarjDevByNoteBook <Ongarj2314@gmail.com>
2023-07-03 16:23:25 +0700

    Edit DbConnect UpdateMethod

--- Models/DbConnect.ini.php
+++ Models/DbConnect.ini.php
@@ -7,8 +7,13 @@ class Connect_Database
     {
         $this->ObjConnect = mysqli_connect("localhost:33886", "root", "root", "");
     }
-
-    public function SelectTable(string $colname = "*", string $TableName = "", string $Sort = "ASC")
+/**
+ * @param string $colname สำหรับใส่ข้อมูล colnameให้ตรงกับฐานข้อมูล
+ * @param string $TableName
+ * @param string $Sort
+ * @return 
+ */
+    public function SelectTable(string $colname = "*", string $TableName = "", string $Sort = "ASC"): object
     {
         if ($TableName != "") {
             $sql = "SELECT " . $colname . " FROM " . $TableName . " ORDER BY " . $Sort;
@@ -18,7 +23,7 @@ class Connect_Database
         return false;
     }
 
-    public function InsertTable(string $TableName, string $colname, $Data)
+    public function InsertTable(string $TableName, string $colname, $Data) : bool
     {
         $escapedData = intval($Data);
         if ($TableName != "") {
@@ -29,16 +34,19 @@ class Connect_Database
         return false;
     }
 
-    public function UpdateTable(string $TableName, string $colname_Data, string $Where)
+    public function UpdateTable(string $TableName, string $colname_Data, string $Where) : bool
     {
         $sql = "UPDATE " . $TableName . " SET " . $colname_Data . " WHERE " . $Where;
         return mysqli_query($this->ObjConnect, $sql);
     }
 
-    public function DeleteTable(string $TableName, string $Where)
+    public function DeleteTable(string $TableName, string $Where) : bool
     {
         $sql = "DELETE FROM " . $TableName . " WHERE " . $Where;
         return mysqli_query($this->ObjConnect, $sql);
     }
+    // public function InnerJoin(){
+    //     $sql = "INNER JOIN"
+    // }
 }
 ?>
