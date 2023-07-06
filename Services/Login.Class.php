b2efe5f61879c439ccd83fd9caeca3cec35e7e52
OnarjDevByNoteBook <Ongarj2314@gmail.com>
2023-07-06 16:41:28 +0700

    Add js

--- Services/Login.Class.php
+++ Services/Login.Class.php
@@ -1,4 +1,9 @@
 <?php
+if($_REQUEST['action_get'] == "Login"){
+    $this->Login();
+}else if($_REQUEST['action_get'] == "Verify"){
+    $this->VerifyAndSyncData();
+}
 Class Login{
     public $DataBase;
     public function __construct()
@@ -6,17 +11,22 @@ Class Login{
         require("../Models/DbConnect.ini.php");
         $DataBase = new Connect_Database();
         session_start();
-        if($_REQUEST['action_get'] == "Login"){
-
-        }else if($_REQUEST['action_get'] == "Verify"){
-            $this->DataBase->SelectTable("IdAll,NameAll,RoleAll,WaitPermision_SCR,Suspension_SCR,")
-        }
     }
-    public function Login(string $UserName,string $Password){
+    public function Login(){
+        $UserName = $_REQUEST['UserName_Post'];
+        $Password = $_REQUEST['Password_Post'];
+        $result = $this->DataBase->SelectTable("IdAll,NameAll,RoleAll,WaitPermision_SCR,Suspension_SCR","users","Where NameAll = '$UserName' AND PasswordAll = '$Password'")->fetch_assoc();
+        if($result->num_rows == 1){
 
+        }else{
+            
+        }
     }
     public function VerifyAndSyncData(){
         $_REQUEST['id_user_session'];
+    }
+    public function Logout(){
+        session_destroy();
     }
 }
 ?>
--- Views/login.php
+++ Views/login.php
@@ -1,11 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>เข้าสู่ระบบ</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../Src/Bootstrap/css/Bootstrap.min.css">
+  <title>เข้าสู่ระบบ</title>
 </head>
+
 <body>
-    
+  <div class="container">
+    <div class="row">
+      <div class="col-4"></div>
+      <div class="col-4 mt-5">
+        <main class="form-signin">
+          <form action="../Services/Login.Class.php" method="POST">
+          <img class="mb-43 rounded-circle mx-auto d-block" src="../Src/Images/Login.jpg"width="120" height="120">
+            <h1 class="h3 mb-3 fw-normal text-center">เข้าสู่ระบบ</h1>
+            <div class="form-floating">
+              <input type="text" class="form-control mb-2" id="floatingInput" name="UserName">
+              <label for="floatingInput">อีเมล</label>
+            </div>
+            <div class="form-floating">
+              <input type="password" class="form-control mb-2" id="floatingPassword" name="Password">
+              <label for="floatingPassword">รหัสผ่าน</label>
+            </div>
+            <button class="w-100 btn btn-lg btn-primary" type="submit">เข้าสู่ระบบ</button>
+            <p class="mt-2 text-center">สำหรับ ผู้ใช้/ร้านค้า/ผู้ส่ง/ผู้ดูแลระบบ</p>
+          </form>
+        </main>
+      </div>
+      <div class="col-4"></div>
 </body>
+
 </html>
